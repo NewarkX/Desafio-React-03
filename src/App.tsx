373f2756952da1a605a76f45d1cede4ea5fbb1fd
NewarkX <brunowmanha@gmail.com>
2021-02-07 09:13:53 -0300

    project created and configurated with bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+
+
+function App() {
+  return (
+    <h1>Hello world</h1>
+  );
+}
+
+export default App;
